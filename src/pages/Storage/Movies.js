4e83ed69d3171e3aa1eea3e4bc9679d5116d800d
neeleshrambally <ramballyn@gmail.com>
2023-08-09 00:28:35 +0200

    init

--- src/pages/Storage/Movies.js
+++ src/pages/Storage/Movies.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import GridWrapper from '../../components/common/GridWrapper/GridWrapper'
 import BasicSnackbar from '../../components/common/BasicSnackbar/BasicSnackbar';
-import UserTable from '../../components/UserTable/UserTable';
+import MovieTable from '../../components/MovieTable/MovieTable'
 import BasicCard from '../../components/common/BasicCard/BasicCard';
 
 const Movies = () => {
@@ -17,7 +17,7 @@ const Movies = () => {
     return (
       <GridWrapper>
         <BasicCard 
-          content={<UserTable onError={() => setOpen(true)} />}
+          content={<MovieTable onError={() => setOpen(true)} />}
         />
         <BasicSnackbar
           open={open}
